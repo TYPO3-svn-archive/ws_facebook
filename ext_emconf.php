4a74a5d3be6dc0c8723383cff9604c3f1565930a
okolya <okolya@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-27 13:12:07 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ws_facebook/trunk@51466 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,10 +2,16 @@
 
 ########################################################################
 # Extension Manager/Repository config file for ext "ws_facebook".
+#
+# Auto generated 27-08-2011 13:48
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Facebook plugins',
+	'title' => 'Facebook plugin',
 	'description' => 'Shows the l­ast activity from public wall of Facebook page',
 	'category' => 'plugin',
 	'author' => 'Nikolay Orlenko',
@@ -31,7 +37,10 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 		'suggests' => array(
 		),
-	)
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"d3d8";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"56a3";s:17:"ext_localconf.php";s:4:"7313";s:14:"ext_tables.php";s:4:"d355";s:16:"locallang_db.xml";s:4:"b3da";s:14:"doc/manual.sxw";s:4:"8f4f";s:26:"Library/facebook/readme.md";s:4:"5998";s:33:"Library/facebook/src/facebook.php";s:4:"0b69";s:43:"Library/facebook/src/fb_ca_chain_bundle.crt";s:4:"c305";s:32:"Library/facebook/tests/tests.php";s:4:"de86";s:31:"pi1/class.tx_wsfacebook_pi1.php";s:4:"eca4";s:19:"pi1/flexform_ds.xml";s:4:"7029";s:17:"pi1/locallang.xml";s:4:"4103";s:19:"pi1/res/up-link.gif";s:4:"4a0e";}',
 );
 
 ?>
